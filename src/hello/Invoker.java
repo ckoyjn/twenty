b943031043c99326887e4d41910216b47c586462
caokai <>
2016-02-21 20:26:36 +0800

    Command模式3

---
+++ src/hello/Invoker.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Invoker {
+    
+    private Command command;
+   
+    public Invoker(Command command) {
+     
+        this.command = command;
+        
+    }
+
+    public void action() {
+     
+        command.execute();
+        
+    }
+    
+}
