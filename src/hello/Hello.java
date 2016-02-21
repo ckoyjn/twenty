05d7c1843d52f0d247bc02194a43ad8ce72f3883
caokai <>
2016-02-21 20:02:47 +0800

    main方法3

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -21,6 +21,7 @@ public class Hello {
         public static void main(String[] args){}
         public static void main(String[] args){}
         public static void main(String[] args){}
+        public static void main(String[] args){}
     }
     
 }
