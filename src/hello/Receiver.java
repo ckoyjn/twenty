3e2d663009444b8f451125ebbe6a29ec908aa0c2
caokai <>
2016-02-21 22:22:07 +0800

    7

--- src/hello/Receiver.java
+++ src/hello/Receiver.java
@@ -6,20 +6,20 @@
 package hello;
 
 /**
- * 6
+ * 7
  * @author Administrator
  */
 
 public class Receiver {
-   
+ 
     public Receiver() {
         
         //write code here
         
     }
-  
+
     public void action() {
-        
+      
         System.out.println("Action has been taken");
         
     }
