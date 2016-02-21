22dc7de8f36fd8abb0fee302a3a2c111462f3526
caokai <>
2016-02-21 21:04:37 +0800

    1

---
+++ src/hello/ConcreteCommand.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello;
+
+/**
+ * 1
+ * @author Administrator
+ */
+
+public class ConcreteCommand implements Command {
+   
+    private Receiver receiver;
+ 
+    public ConcreteCommand(Receiver receiver) {
+      
+        this.receiver = receiver;
+        
+    }
+   
+    public void execute() {
+     
+        receiver.action();
+        
+    }
+}
