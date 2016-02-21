b3139d4689e17990c41d89028c3c288633f5a0a5
caokai <>
2016-02-21 23:05:19 +0800

    8

--- src/hello/Invoker.java
+++ src/hello/Invoker.java
@@ -6,20 +6,20 @@
 package hello;
 
 /**
- * 7
+ * 8
  * @author Administrator
  */
 
 public class Invoker {
-    
+
     private Command command;
-    
+   
     public Invoker(Command command) {
-      
+     
         this.command = command;
         
     }
-    
+
     public void action() {
         
         command.execute();
