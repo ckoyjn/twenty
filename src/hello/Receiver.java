f6a38d50f54034c51c1d3b26516268a604ff0dd7
caokai <>
2016-02-21 20:55:54 +0800

    1

---
+++ src/hello/Receiver.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello;
+
+/**
+ * 1
+ * @author Administrator
+ */
+
+public class Receiver {
+    
+    public Receiver() {
+        //write code here
+    }
+  
+    public void action() {
+     
+        System.out.println("Action has been taken");
+
+    }
+
+}
