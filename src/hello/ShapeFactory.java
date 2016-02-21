720e4ad35d0563b00def8f4fdb97d766e7ba8a40
caokai <>
2016-02-22 00:08:08 +0800

    1

---
+++ src/hello/ShapeFactory.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello;
+
+/**
+ * 1
+ * @author Administrator
+ */
+
+public class ShapeFactory {
+    //使用 getShape 方法获取形状类型的对象
+
+    public Shape getShape(String shapeType) {
+        if (shapeType == null) {
+            return null;
+        }
+        if (shapeType.equalsIgnoreCase("CIRCLE")) {
+            return new Circle();
+        } else if (shapeType.equalsIgnoreCase("RECTANGLE")) {
+            return new Rectangle();
+        } else if (shapeType.equalsIgnoreCase("SQUARE")) {
+            return new Square();
+        }
+        return null;
+    }
+}
