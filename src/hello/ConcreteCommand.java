a805b85c08611ee919758d2b3326748103681164
caokai <>
2016-02-21 21:55:27 +0800

    3

--- src/hello/ConcreteCommand.java
+++ src/hello/ConcreteCommand.java
@@ -6,22 +6,22 @@
 package hello;
 
 /**
- * 3
+ * 4
  * @author Administrator
  */
 
 public class ConcreteCommand implements Command {
-    
+ 
     private Receiver receiver;
-  
+    
     public ConcreteCommand(Receiver receiver) {
-        
+       
         this.receiver = receiver;
         
     }
-  
+   
     public void execute() {
-    
+        
         receiver.action();
         
     }
