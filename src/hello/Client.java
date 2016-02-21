2324c577f1937b4b30b017c24931fd7e898b08bf
caokai <>
2016-02-21 20:20:52 +0800

    Command模式2

---
+++ src/hello/Client.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello;
+
+/**
+ *1
+ * @author Administrator
+ */
+
+public class Client {
+
+    public static void main(String[] args) {
+
+        Receiver receiver = new Receiver();
+
+        Command command = new ConcreteCommand(receiver);
+
+        Invoker invoker = new Invoker(command);
+        
+        invoker.action();
+        
+    }
+}
